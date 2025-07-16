ac479b066f69c49d5243a3297863f859e81c07fb
Hina Shafique <hinashafique4159@gmail.com>
2025-07-16 10:58:18 +0500

    types in js

---
+++ episode-02/types.js
@@ -0,0 +1,34 @@
+// dynamic typed values like php
+
+typeof decValue; // return string of type of value
+
+//undefined
+var decValue; // undefined because we don'y assign it a vlaue
+
+// string
+
+var doubleQuoted = "double quoted wasn't  "; //stringe
+var singleQuoted = 'single quoted "quotes use" '; //stringe
+
+//number (in javascript there is no distinction between integers and floats) all are numbers
+
+var num1 = 1; //number
+var num2 = 1.23; //number
+
+//boolean
+
+var trueValue = true; // boolean
+var falseValue = false; // boolean
+
+//object
+
+var obj = {}; //object
+var nullValue = null; // null is an object in javascript 
+
+//array (only index no associative arrays)
+
+var arr = []; //object (array is a type of object in javascript)
+
+//functions
+
+function fn(){}; //function
